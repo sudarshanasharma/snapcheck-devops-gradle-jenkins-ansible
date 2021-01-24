917702dda86cc8ec95f85ebb2c68e0f142857cd5
sudarshanasharma <72416797+sudarshanasharma@users.noreply.github.com>
2021-01-24 01:05:58 -0800

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,23 +4,19 @@ pipeline {
 		
 		stage('install gradle and upgrade gradlew'){
 			steps {
-				script {
-					withGradle{
-				sh 'gradle wrapper --gradle-version 6.6.1'
+					gradle{
+						 useWrapper true
+				         sh 'gradle wrapper --gradle-version 6.6.1'
 					}
-
-			      }
 			}
 		}
-		
-		
 	  stage('Build project') {
               steps {
-		      script {
-			      withGradle{
+			      gradle{
+			          useWrapper true
 				      sh './gradlew build'		  
 			      }
-		          }
+		
               }
          }
     }
